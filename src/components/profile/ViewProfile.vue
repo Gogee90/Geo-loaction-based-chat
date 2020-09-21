553495f28629fbe293c08e6c7de3653eae4fc603
Gogee90 <dead69-not@yandex.ru>
2020-09-21 13:28:49 +0500

    CORS problem has been fixed

--- src/components/profile/ViewProfile.vue
+++ src/components/profile/ViewProfile.vue
@@ -3,7 +3,10 @@
     <div v-if="profile" class="card">
       <h2 class="deep-purple-text center">{{ profile.alias}}'s Wall</h2>
       <ul class="comments collection">
-        <li>Comment</li>
+        <li v-for="(comment, index) in comments" :key="index">
+          <div class="deep-purple-text">{{ comment.from }}</div>
+          <div class="grey-text text-darken-2">{{ comment.content }}</div>
+        </li>
       </ul>
       <form @submit.prevent="addComment">
         <div class="field">
@@ -26,7 +29,8 @@
                 profile: null,
                 newComment: null,
                 feedback: null,
-                user: null
+                user: null,
+                comments: []
             }
         },
         created() {
@@ -44,6 +48,18 @@
                 .then(user => {
                     this.profile = user.data()
                 })
+
+            db.collection('comments').where('to', '==', this.$route.params.id).orderBy('time')
+            .onSnapshot((snapshot) => {
+              snapshot.docChanges().forEach(change => {
+                if(change.type == 'added'){
+                  this.comments.unshift({
+                    from: change.doc.data().from,
+                    content: change.doc.data().content
+                  })
+                }
+              })
+            })
         },
         methods: {
             addComment() {
@@ -66,6 +82,18 @@
     }
 </script>
 
-<style scoped>
-
+<style>
+.view-profile .card{
+  padding: 20px;
+  margin-top: 60px;
+}
+.view-profile h2{
+  font-size: 2em;
+  margin-top: 0;
+}
+.view-profile li{
+  padding: 10px;
+  border-bottom: 1px solid #eee
+}
 </style>
+
